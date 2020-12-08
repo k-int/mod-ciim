c316a97dde0516a75bbc93e1994ba3a959d8e73f
Ian Ibbotson <ianibbo@gmail.com>
2020-12-08 19:32:06 +0000

    Enumerate inventory

--- spikes/enumerate_inv.groovy
+++ spikes/enumerate_inv.groovy
@@ -40,10 +40,17 @@ password=null;
 tenant=null;
 jwt=null;
 folio_api = null;
-session_ctx=[:]
+session_ctx=[
+  auth:'',
+  refdata:[:]
+]
 
 this.configure('cardinal_si');
 this.login();
+this.loadRefdata('IdentifierTypes');
+this.loadRefdata('Classns');
+this.loadRefdata('ContributorNameTypes');
+this.loadRefdata('InstanceTypes');
 this.enumerateInventory();
 
 System.exit(0);
@@ -88,7 +95,7 @@ def login() {
         println("Problem ${body} ${fs} ${fs.getStatusCode()}");
       }
       response.success { FromServer fs, Object body ->
-        println("OK ${body} ${fs} ${fs.getStatusCode()}");
+        // println("OK ${body} ${fs} ${fs.getStatusCode()}");
         println("Logged In")
         def tok_header = fs.headers?.find { h-> h.key == 'x-okapi-token' }
         if ( tok_header ) {
@@ -107,9 +114,27 @@ def login() {
   }
 }
 
+
 def enumerateInventory() {
+  String cursor = '0001-01-01T00:00:00.000'
+  Map r = null;
+  int c = 0;
+  // Artificially restrict ourselves to 5 pages of data
+  while ( c++ < 5 ) {
+    println("Fetch page of data starting at ${cursor}");
+    r = getPageOfInventoryDataSince(cursor)
+    cursor = r.maxCursor;
+
+    // Don't ddos 
+    Thread.sleep(5000);
+  }
+
+}
+
+def getPageOfInventoryDataSince(String cursor) {
 
   def fc = this.getClient()
+  Map result = [:]
 
   fc.get {
     request.uri.path='/instance-storage/instances'
@@ -117,13 +142,105 @@ def enumerateInventory() {
     request.headers.'X-Okapi-Token'=this.session_ctx.auth
     request.headers.'accept'='application/json'
     request.uri.query= [
-      'metadata.updatedDate':'0001-01-01T00:00:00.000'
+      'query':'metadata.updatedDate>'+cursor
     ]
     response.failure { FromServer fs, Object body ->
       println("Problem ${body} ${fs} ${fs.getStatusCode()}");
     }
     response.success { FromServer fs, Object body ->
-      println("OK ${body} ${fs} ${fs.getStatusCode()}");
+      // println("OK ${body} ${fs} ${fs.getStatusCode()}");
+      println("Total records: ${body.totalRecords}");
+      body.instances.each { inst ->
+        println("\n\nInstance:");
+        inst.each { k,v -> 
+          if ( v instanceof String ) {
+            println("  ${k} -> ${v}");
+          }
+          else if ( k == 'identifiers' ) {
+            println("  Identifiers");
+            inst.identifiers?.each { id ->
+              // Resolve identifierTypeId using previously looked up refdata
+              println("    [${this.session_ctx.refdata.IdentifierTypes[id.identifierTypeId]?.name}] ${id.value}");
+            }
+          }
+          else if ( k == 'contributors' ) {
+            println("  Contributors");
+            inst.contributors?.each { id ->
+              // Resolve identifierTypeId using previously looked up refdata
+              println("    [${this.session_ctx.refdata.ContributorNameTypes[id.contributorNameTypeId]?.name}] ${id.name}");
+            }
+          }
+          else if ( k == 'subjects' ) {
+            println("  Subjects");
+            inst.subjects?.each { subject ->
+              println("    ${subject}");
+            }
+          }
+          else {
+            println("  ${k} -> ${v}")
+          }
+        }
+        println("UpdatedDate: ${inst.metadata.updatedDate}");
+        result.maxCursor=inst.metadata.updatedDate;
+      }
     }
   }
+
+  return result;
+}
+
+
+//
+// https://folio-snapshot-okapi.dev.folio.org/identifier-types?limit=1000&query=cql.allRecords=1%20sortby%20name
+Map loadRefdata(String type) {
+
+  Map result = session_ctx.refdata.get(type);
+
+  if ( result == null ) {
+    result = [:]
+    session_ctx.refdata[type]=result;
+
+    String service_path = null;
+    switch ( type ) {
+      case 'IdentifierTypes':
+        service_path = '/identifier-types'
+        response_key='identifierTypes'
+        break;
+      case 'Classns':
+        service_path = '/classification-types'
+        response_key='classificationTypes'
+        break;
+      case 'ContributorNameTypes':
+        service_path = '/contributor-name-types'
+        response_key='contributorNameTypes'
+        break;
+      case 'InstanceTypes':
+        service_path = '/instance-types'
+        response_key='instanceTypes'
+        break;
+    }
+
+    if ( service_path != null ) {
+      this.getClient().get {
+        request.uri.path = service_path
+        request.headers.'X-Okapi-Tenant'=this.tenant;
+        request.headers.'X-Okapi-Token'=this.session_ctx.auth
+        request.headers.'accept'='application/json'
+        request.uri.query= [
+          'limit':500,
+          'query':'cql.allRecords=1%20sortby%20name'
+        ]
+        response.failure { FromServer fs, Object body ->
+          println("Problem ${body} ${fs} ${fs.getStatusCode()}");
+        }
+        response.success { FromServer fs, Object body ->
+          body[response_key].each { refdata ->
+            result[refdata.id] = refdata;
+          }
+        }
+      }
+    }
+  }
+
+  return result;
 }

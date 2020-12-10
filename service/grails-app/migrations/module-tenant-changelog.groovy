e5065d493881c51de670381d83005de654c0dd2c
Ethan Freestone <ethan.freestone@k-int.co.uk>
2020-12-10 14:38:15 +0000

    added appSettings to startup, added permission to MD

--- service/grails-app/migrations/module-tenant-changelog.groovy
+++ service/grails-app/migrations/module-tenant-changelog.groovy
@@ -2,5 +2,6 @@ databaseChangeLog = {
   include file: 'initial-customisations.groovy'
   include file: 'setup-refdata.groovy'
   include file: 'setup-custom-properties.groovy'
+  include file: 'setup-app-settings.groovy'
   include file: 'create-mod-ciim.groovy'
 }

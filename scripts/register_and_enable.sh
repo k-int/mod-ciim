07b250bd8beffced9486981c4a77b882592b2d96
Ian Ibbotson <ianibbo@gmail.com>
2020-12-11 10:53:46 +0000

    *blush*

--- scripts/register_and_enable.sh
+++ scripts/register_and_enable.sh
@@ -15,9 +15,13 @@ DEP_DESC=`cat ${DESCRIPTORDIR}/DeploymentDescriptor.json`
 SVC_ID=`echo $DEP_DESC | jq -rc '.srvcId'`
 INS_ID=`echo $DEP_DESC | jq -rc '.instId'`
 
+echo Delete previous install
+
 curl -XDELETE "http://localhost:9130/_/proxy/tenants/diku/modules/${SVC_ID}"
 curl -XDELETE "http://localhost:9130/_/discovery/modules/${SVC_ID}/${INS_ID}"
 curl -XDELETE "http://localhost:9130/_/proxy/modules/${SVC_ID}"
+
+echo Install new module
 # ./gradlew clean generateDescriptors
 curl -XPOST 'http://localhost:9130/_/proxy/modules' -d @"${DESCRIPTORDIR}/ModuleDescriptor.json"
 curl -XPOST 'http://localhost:9130/_/discovery/modules' -d "$DEP_DESC"

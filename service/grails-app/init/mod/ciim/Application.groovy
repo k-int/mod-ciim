37478ef557e7ad8ac6f3badbf33dc4baf072e914
Ian Ibbotson <ianibbo@gmail.com>
2021-05-14 17:18:55 +0100

    Bump grails-okapi and web-toolkit dependencies, changes to Application class use spring @Bean annotation which will cause application to properly pick up customizer bean, update loggging pattern to include tenant

--- service/grails-app/init/mod/ciim/Application.groovy
+++ service/grails-app/init/mod/ciim/Application.groovy
@@ -6,9 +6,13 @@ import grails.plugins.DefaultGrailsPluginManager
 import groovy.transform.CompileStatic
 import org.springframework.boot.web.embedded.undertow.UndertowBuilderCustomizer
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory
-import io.micronaut.context.annotation.Bean
 import io.undertow.Undertow.Builder
+import io.undertow.UndertowOptions
+import org.springframework.context.annotation.Bean
+import groovy.util.logging.Slf4j
 
+@CompileStatic
+@Slf4j
 class Application extends GrailsAutoConfiguration {
 
   static void main(String[] args) {
@@ -23,7 +27,8 @@ class Application extends GrailsAutoConfiguration {
   }
 
   @Bean
-  UndertowServletWebServerFactory embeddedServletContainerFactory(){
+  public UndertowServletWebServerFactory embeddedServletContainerFactory() {
+
     UndertowServletWebServerFactory factory = new UndertowServletWebServerFactory()
     factory.builderCustomizers << new UndertowBuilderCustomizer() {
 
@@ -31,9 +36,11 @@ class Application extends GrailsAutoConfiguration {
       public void customize(Builder builder) {
 
         // Min of 2, Max of 4 I/O threads
-        builder.ioThreads = Math.min(Math.max(Runtime.getRuntime().availableProcessors(), 2), 4)
+        final int ioThreadCount = Math.min(Math.max(Runtime.getRuntime().availableProcessors(), 2), 4)
+        builder.ioThreads = ioThreadCount
 
-        final int heap_coef = (Runtime.getRuntime().maxMemory() / 1024 / 1024)/256
+
+        final int heap_coef = ((Runtime.getRuntime().maxMemory() / 1024 / 1024)/256) as int
         int workers_per_io = 8
         if (heap_coef <= 2) {
           workers_per_io = 6
@@ -42,18 +49,21 @@ class Application extends GrailsAutoConfiguration {
         }
 
         // 8 Workers per I/O thread
-        builder.workerThreads = builder.workerThreads = builder.ioThreads * workers_per_io
-
+        final int workerThreadCount = ioThreadCount * workers_per_io
+        builder.workerThreads = workerThreadCount
         // Enable HTTP2
-        builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true)
+        // builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true)
 
-        log.info "Runtime memory reported ${Runtime.getRuntime().maxMemory() / 1024 / 1024} mb"
-        log.info "Runtime CPUs reported ${Runtime.getRuntime().availableProcessors()}"
-        log.info "Allocated ${builder.ioThreads} IO Threads"
-        log.info "Allocated ${builder.workerThreads} worker threads"
-        log.info "Allocated ${builder.bufferSize} bytes of ${builder.directBuffers ? 'direct' : 'indirect'} buffer space per thread"
+        println "Runtime memory reported ${Runtime.getRuntime().maxMemory() / 1024 / 1024} mb"
+        println "Runtime CPUs reported ${Runtime.getRuntime().availableProcessors()}"
+        println "Allocated ${ioThreadCount} IO Threads"
+        println "Allocated ${workerThreadCount} worker threads"
+
       }
     }
+
+    return factory
   }
+
 
 }
